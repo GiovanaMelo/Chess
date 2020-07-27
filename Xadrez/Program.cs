c9e450c7fedf267945de42159c1b3dd049b69091
Giovana <giovanamelomr@gmail.com>
2020-07-27 11:12:30 -0300

    Criando a posXadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -12,17 +12,19 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            /* try
+             {
+                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-                Tela.imprimirTabuleiro(tab);
-            }catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                 Tela.imprimirTabuleiro(tab);
+             }catch(TabuleiroException e)
+             {
+                 Console.WriteLine(e.Message);
+             }*/
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }
