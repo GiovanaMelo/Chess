d3e48859e71b54dedcf13d3d069688adb03e5250
Giovana <giovanamelomr@gmail.com>
2020-07-29 10:52:29 -0300

    Implementando as restricoes de movimento

--- Xadrez/Tabuleiro/xadrez/PartidaDeXadrez.cs
+++ Xadrez/Tabuleiro/xadrez/PartidaDeXadrez.cs
@@ -10,15 +10,15 @@ namespace xadrez
     class PartidaDeXadrez
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada { get; private set; }
 
         public PartidaDeXadrez()
         {
             tab = new Tabuleiro(8, 8);
             turno = 1;
-            jogadorAtual = Cor.Verde;
+            jogadorAtual = Cor.Amarelo;
             terminada = false;
             colocarPecas();
         }
@@ -29,6 +29,46 @@ namespace xadrez
             p.incrementarQntMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
+        }
+
+        public void realizaJogada(Posicao origem, Posicao destino)
+        {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+        public void validarPosicaoDeOrigem(Posicao pos)
+        {
+            if(tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peça nessa posição.");
+            }if(jogadorAtual != tab.peca(pos).cor)
+            {
+                throw new TabuleiroException("A peça escolhida não é sua!");
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não existe movimentos possíveis para esta peça.");
+            }
+        }
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
+        {
+            if (!tab.peca(origem).podeMoverPara(destino))
+            {
+                throw new TabuleiroException("Posição de destino não é valida.");
+            }
+        }
+
+        private void mudaJogador()
+        {
+            if(jogadorAtual == Cor.Amarelo)
+            {
+                jogadorAtual = Cor.Verde;
+            }
+            else
+            {
+                jogadorAtual = Cor.Amarelo;
+            }
         }
         private void colocarPecas()
         {
