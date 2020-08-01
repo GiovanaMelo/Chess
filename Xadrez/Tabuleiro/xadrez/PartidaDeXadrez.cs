bb7456eac250af5bbb5ddc35d2c31db1d26e2907
Giovana <giovanamelomr@gmail.com>
2020-08-01 13:16:11 -0300

    Jogada especial promocao

--- Xadrez/Tabuleiro/xadrez/PartidaDeXadrez.cs
+++ Xadrez/Tabuleiro/xadrez/PartidaDeXadrez.cs
@@ -140,6 +140,20 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce nao pode se colocar em xeque.");
             }
+            Peca p = tab.peca(destino);
+
+            //jogada especial promocao
+            if(p is Peao)
+            {
+                if (p.cor == Cor.Amarelo && destino.linha == 0 || p.cor == Cor.Verde && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -157,7 +171,7 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
+            
 
             //jogada especial en passant
             if(p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
